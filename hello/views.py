3c7c44ff1153a07132bdf7da3bc3b9e9f4399643
kiote <krivich.ekaterina@gmail.com>
2020-04-24 14:33:24 +0300

    button

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,8 @@ from .models import Greeting
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    telega_login = '<script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-login="GroundWorkerBot" data-size="large" data-auth-url="https://polar-plains-99906.herokuapp.com/" data-request-access="write"></script>'
+    return HttpResponse('<pre>' + r.text + '</pre>' + telega_login)
 
 def db(request):
 
