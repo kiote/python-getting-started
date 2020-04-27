a4a7b9eff5861ad8a1ff1369240df672b7c17e70
kiote <krivich.ekaterina@gmail.com>
2020-04-27 14:38:36 +0300

    add menu

--- hello/views.py
+++ hello/views.py
@@ -14,12 +14,8 @@ from .forms import MessageForm
 logger = logging.getLogger(__name__)
 send_message_url = 'https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s'
 
-# Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    telega_login = '<script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-login="GroundWorkerBot" data-size="large" data-auth-url="https://polar-plains-99906.herokuapp.com/login" data-request-access="write"></script>'
-    return HttpResponse('<pre>' + r.text + '</pre>' + telega_login)
+    return render(request, 'index.html')
 
 def send_message(request):
     user_id = request.COOKIES['Telegram']
@@ -49,6 +45,7 @@ def login(request):
         response.set_cookie('Telegram', str(user_id))
     return response
 
+# receiving messages from telegram here
 def message(request):
     message = request.body
     d = json.loads(message)
