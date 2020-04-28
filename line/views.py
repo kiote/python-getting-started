800420435b655c43566b71c151de5658b4d37fc3
kiote <krivich.ekaterina@gmail.com>
2020-04-28 12:08:20 +0300

    add logging

--- line/views.py
+++ line/views.py
@@ -10,15 +10,17 @@ from .models import Message
 
 def message_callback(request):
     raw_message = request.body
-    d = json.loads(raw_message)
-    message_user = d['source']['userId']
-    message_reply_token = d['replyToken']
-    message_text = d['message']['text']
+    # d = json.loads(raw_message)
+    # message_user = d['source']['userId']
+    # message_reply_token = d['replyToken']
+    # message_text = d['message']['text']
 
-    db_message = Message(user=message_user,
-                         message=message_text,
-                         raw_message=raw_message,
-                         reply_token=message_reply_token)
-    db_message.save()
+    # db_message = Message(user=message_user,
+    #                      message=message_text,
+    #                      raw_message=raw_message,
+    #                      reply_token=message_reply_token)
+    # db_message.save()
+
+    logger.error(message_raw.decode("utf-8"))
 
     return HttpResponse('ok')
