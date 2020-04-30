097de2e7a95efdb06373ae6b144566955a9a1733
kiote <krivich.ekaterina@gmail.com>
2020-04-30 10:37:14 +0300

    save message

--- viber/views.py
+++ viber/views.py
@@ -6,6 +6,8 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Message
+
 logger = logging.getLogger(__name__)
 viber_api_url = 'https://chatapi.viber.com/pa/'
 
@@ -14,4 +16,16 @@ from django.shortcuts import render
 def callback_url(request):
     raw_message = request.body
     logger.error(raw_message)
+    if raw_message['event'] == 'message':
+        message_text = raw_message['message']['text']
+        message_token = raw_message['message_token']
+        sender_id = raw_message['sender']['id']
+        created = raw_message['timestamp']
+        db_message = Message(created=created,
+                             sender_id=sender_id,
+                             token=message_token,
+                             raw_message=raw_message,
+                             text=message_text,
+                            )
+        db_message.save()
     return HttpResponse('ok')
