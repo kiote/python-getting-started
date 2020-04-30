7b581bf29983148aa0c13b53f707112c07dd9c4b
kiote <krivich.ekaterina@gmail.com>
2020-04-30 10:49:52 +0300

    add field

--- viber/models.py
+++ viber/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Message(models.Model):
-    # created = models.IntegerField(default=0)
+    created = models.IntegerField(default=0)
     sender_id = models.CharField(max_length=80, default="")
     token = models.CharField(max_length=200, default="")
     text = models.TextField(default="")
